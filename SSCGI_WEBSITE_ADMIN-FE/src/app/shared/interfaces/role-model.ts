269defec5f33d6555386d1dedc2185c1ea3e7675
John Lorenz Jenoves <jljenoves@sscgi.com>
2024-08-29 02:24:41 +0000

    Updated role-model.ts

--- SSCGI_WEBSITE_ADMIN-FE/src/app/shared/interfaces/role-model.ts
+++ SSCGI_WEBSITE_ADMIN-FE/src/app/shared/interfaces/role-model.ts
@@ -1,14 +1,4 @@
 export interface PolicyOption {
-<<<<<<< HEAD
-    name: string;
-    checked: boolean;
-    options: {
-      Create: boolean;
-      Edit: boolean;
-      View: boolean;
-      Delete: boolean;
-      Restore: boolean;
-=======
   rolePolicyName: string;
   isChecked: boolean;
     options: {
@@ -17,17 +7,10 @@ export interface PolicyOption {
       canView: boolean;
       canDelete: boolean;
       canRestore: boolean;
->>>>>>> master
     };
   }
   
   export interface RoleData {
-<<<<<<< HEAD
-    roleCode: string;  // Updated from 'code'
-    roleName: string;
-    description: string;
-    policies: PolicyOption[];
-=======
     roleId:number;
     roleCode: string; 
     roleName: string;
@@ -35,5 +18,4 @@ export interface PolicyOption {
     policies: PolicyOption[];
 
 
->>>>>>> master
   }
