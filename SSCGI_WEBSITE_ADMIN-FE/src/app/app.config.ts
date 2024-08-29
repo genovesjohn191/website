18a3c3ca43a73fd241ea84b9eaf164d87a9949c4
John Lorenz Jenoves <jljenoves@sscgi.com>
2024-08-29 02:28:32 +0000

    Updated app.config.ts

--- SSCGI_WEBSITE_ADMIN-FE/src/app/app.config.ts
+++ SSCGI_WEBSITE_ADMIN-FE/src/app/app.config.ts
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-=======
 import { ApplicationConfig, provideZoneChangeDetection,importProvidersFrom } from '@angular/core';
->>>>>>> master
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-<<<<<<< HEAD
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
-=======
 import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FilterBySearchPipe } from './shared/components/crudmodal/filter-by-search.pipe';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),importProvidersFrom(HttpClientModule,MatSnackBarModule,FilterBySearchPipe)]
->>>>>>> master
 };
